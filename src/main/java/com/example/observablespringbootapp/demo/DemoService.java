55f1f7678fcf475fac86d17c42e1a40cecbcbd23
Sakibul_Munna <sakibul.munnaa@gmail.com>
2023-09-24 20:36:14 +0600

    Using withSpan annotation in the service for generating a custom trace.

--- src/main/java/com/example/observablespringbootapp/demo/DemoService.java
+++ src/main/java/com/example/observablespringbootapp/demo/DemoService.java
@@ -1,5 +1,6 @@
 package com.example.observablespringbootapp.demo;
 
+import io.opentelemetry.instrumentation.annotations.WithSpan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -8,6 +9,10 @@ import org.springframework.stereotype.Service;
 public class DemoService {
     private static final Logger log = LoggerFactory.getLogger(DemoService.class);
 
+    /**
+     * This function shows the annotation way to create custom span.
+     */
+    @WithSpan
     public DemoResponse buildResponseForSayHello(){
         log.info("Here inside service");
         return new DemoResponse("Hello World");
