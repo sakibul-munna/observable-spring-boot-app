67955a98532bdfb886ba77f074e758610d700c1c
Sakibul_Munna <sakibul.munnaa@gmail.com>
2023-09-24 09:34:04 +0600

    Using custom DemoResponse for API responses.

--- src/main/java/com/example/observablespringbootapp/demo/DemoController.java
+++ src/main/java/com/example/observablespringbootapp/demo/DemoController.java
@@ -1,10 +1,11 @@
 package com.example.observablespringbootapp.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 @RequestMapping("/api/v1/demo")
@@ -14,7 +15,7 @@ public class DemoController {
     private DemoService demoService;
 
     @GetMapping
-    public ResponseEntity<String> sayHello(){
-        return ResponseEntity.ok().body("Hello There");
+    public DemoResponse sayHello(){
+        return demoService.buildResponseForSayHello();
     }
 }
--- src/main/java/com/example/observablespringbootapp/demo/DemoService.java
+++ src/main/java/com/example/observablespringbootapp/demo/DemoService.java
@@ -4,4 +4,9 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class DemoService {
+
+    public DemoResponse buildResponseForSayHello(){
+        return new DemoResponse("Hello World");
+    }
+
 }
